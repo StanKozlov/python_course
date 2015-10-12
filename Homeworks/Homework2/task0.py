71fafc68ccece1c3ed4c7b71e06cf35130ce0b9c
hamster108 <saliva102@gmail.com>
2015-10-13 00:38:12 +0300

    second homework, beter tasks 0 and 2

--- Homeworks/Homework2/task0.py
+++ Homeworks/Homework2/task0.py
@@ -1,19 +1,20 @@
 def plural (n, a,b,c):
     if n%10 == 1 and n%100!=11:
-        k = (str(n)+' '+ a)
+        k = str(n)+' '+ a
     elif (n%10 == 2 or n%10 == 3 or n%10 == 4) and (n%100 != 12 and n%100 != 13 and n%100 != 14):
-        k = (str(n) + ' ' + b)
+        k = str(n) + ' ' + b
     else:
-        k = (str(n) + ' ' + c)
-    print(k)
+        k = str(n) + ' ' + c
+    return k
 s = input()
 n = int(input())
 if s == 'утюг':
-    plural(n, 'утюг', 'утюга', 'утюгов')
+    m = plural(n, 'утюг', 'утюга', 'утюгов')
 elif s == 'ложка':
-    plural(n, 'ложка', 'ложки', 'ложек')
+    m = plural(n, 'ложка', 'ложки', 'ложек')
 elif s == 'гармошка':
-    plural(n, 'гармошка', 'гармошки', 'гармошек')
+    m = plural(n, 'гармошка', 'гармошки', 'гармошек')
 else:
-    plural(n, 'чайник', 'чайника', 'чайников')
+    m = plural(n, 'чайник', 'чайника', 'чайников')
+print(m)
 
--- Homeworks/Homework2/task2.py
+++ Homeworks/Homework2/task2.py
@@ -2,18 +2,18 @@ def prime(p):
     i = 2
     j = True
     if p == 1:
-        return 'False'
+        return False
     elif p == 2:
-        return 'True'
+        return True
     else:
         while i*i <= p and j != False:
             if p % i == 0:
                 j = False
             i += 1
         if j == True:
-            return 'True'
+            return True
         else:
-            return 'False'
+            return False
 Quantity = int(input())
 for i in range(Quantity):
     n = int(input())
