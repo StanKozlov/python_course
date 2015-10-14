16fa6ffe192680cf324ca8085f08d39d5d7e5a41
hamster108 <saliva102@gmail.com>
2015-10-14 21:12:20 +0300

    second homework, corrected task 4

--- Homeworks/Homework2/task4.py
+++ Homeworks/Homework2/task4.py
@@ -10,10 +10,7 @@ def rpfilter(a, *args):
     for arg in args:
         if euclid(a, arg) == 1:
             lst.append(arg)
-    if lst == []:
-        return None
-    else:
-        return lst
+    return lst
 
 
 s = input()
@@ -21,12 +18,11 @@ numbers = s.split(" ")
 list_of_interest = []
 for i in numbers:
     list_of_interest.append(int(i))
-results = rpfilter(*list_of_interest)   
-if results is not None:
-    for i in results:
+if rpfilter(*list_of_interest) != []:   
+    for i in rpfilter(*list_of_interest):
         print(i, end = ' ')
 else:
-    print(results)
+    print('None')
     
 
 
