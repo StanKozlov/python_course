db7dc3f2aeb94060c6e5a0a74a10e869e74d0bc5
hamster108 <saliva102@gmail.com>
2015-11-07 18:49:14 +0300

    exam, second task

---
+++ Exam1/task2.py
@@ -0,0 +1,21 @@
+from math import factorial
+with open('dict.txt', "r") as input_file:
+    words = input_file.read().split('\n')
+adjectives = 0
+nouns = 0
+verbs = 0
+for thing in words:
+    if thing[len(thing)-2:] == 'yo':
+        adjectives += 1
+    elif thing[len(thing)-2:] == 'ka':
+        nouns += 1
+    else:
+        verbs += 1
+if adjectives == 0 or nouns == 0 or verbs == 0:
+    print(0)
+elif adjectives == 1 and nouns == 1 and verbs == 1:
+    print(1)
+else:
+    k = factorial(adjectives)*nouns*verbs
+    print(k)
+           
