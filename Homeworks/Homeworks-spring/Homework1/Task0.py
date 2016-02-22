fda18998efafdf2472ec27d643c05a869a5a24cd
hamster108 <saliva102@gmail.com>
2016-02-22 13:15:14 +0300

    First spring homework, task0 improved

--- Homeworks/Homeworks-spring/Homework1/Task0.py
+++ Homeworks/Homeworks-spring/Homework1/Task0.py
@@ -1,13 +1,22 @@
-def sum(a, b):
-    try:
-        result = a + b
-        if type(a) is int and type(b) is int:
-            if (a >= 0) and (b >= 0):
-                return result
-            else:
-                raise ValueError
-        else:
-            raise TypeError
-    except TypeError:
-        raise TypeError
+##def sum(a, b):
+##    try:
+##        result = a + b
+##        if type(a) is int and type(b) is int:
+##            if (a >= 0) and (b >= 0):
+##                return result
+##            else:
+##                raise ValueError
+##        else:
+##            raise TypeError
+##    except TypeError:
+##        raise TypeError
 
+def sum(a, b):
+    if type(a) is int and type(b) is int:
+        if (a >= 0) and (b >= 0):
+            return a + b
+        else:
+            raise ValueError
+    else:
+        raise TypeError
+    
