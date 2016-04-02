8dfb8066f3697b84b7e330b55a70e85cfee95a0c
hamster108 <saliva102@gmail.com>
2016-04-02 13:19:04 +0300

    pep8 changes

--- CourseworkSpring/CourseworkSpring.py
+++ CourseworkSpring/CourseworkSpring.py
@@ -4,12 +4,13 @@ import subprocess
 import os.path
 
 
-parser = argparse.ArgumentParser(description='Compressing of the file')
+parser = argparse.ArgumentParser(description='Compressing '
+                                             'of the file')
 parser.add_argument('percent', type=str,
                     help='Enter the ideal size of compression')
 parser.add_argument('file', type=str, help='Path to file of interest')
-parser.add_argument('--new_file', type=str, default=' ', help='Path to compressed file,'
-                                                              'usage = --new_file path')
+parser.add_argument('--new_file', type=str, default=' ',
+                    help='Path to compressed file, usage = --new_file path')
 
 args = parser.parse_args()
 percent = args.percent
@@ -23,20 +24,23 @@ def work_with_dir(path):
             filename = os.path.join(root, name)
             print(filename)
             if filename[-4:] == '.png' or filename[-4:] == '.jpg':
-                operation = 'convert ' + filename + ' -resize ' + percent + '% ' + filename
+                operation = 'convert ' + filename + \
+                            ' -resize ' + percent + '% ' + filename
                 print(subprocess.call(operation, shell=True))
 
 if os.path.exists(file):
     if new_file is not ' ':
         if os.path.isfile(file):
-            operation = 'convert ' + file + ' -resize ' + percent + '% ' + new_file
+            operation = 'convert ' + file + \
+                        ' -resize ' + percent + '% ' + new_file
             print(subprocess.call(operation, shell=True))
         else:
             print('I ignored your "new_file" path')
             work_with_dir(file)
     else:
         if os.path.isfile(file):
-            operation = 'convert ' + file + ' -resize ' + percent + '% ' + file
+            operation = 'convert ' + file + \
+                        ' -resize ' + percent + '% ' + file
             print(subprocess.call(operation, shell=True))
         else:
             work_with_dir(file)
